c825826d9a02f8040da4dfec9bea3ce774f373f9
krishnanshagarwal112 <krishnansh710@gmail.com>
2023-08-29 12:43:13 +0530

    basic features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 pygame.init()
 
 # VARIABLES
@@ -14,8 +14,8 @@ running = True
 
 #Paddles {Paddle 1 : CPU, Paddle 2 : User}
 paddle_width = 20
-paddle_height = 200
-paddle_speed_1 = 7
+paddle_height = 175
+paddle_speed_1 = 9
 paddle_speed_2 = 9
 paddle_score_1 = 0
 paddle_score_2 = 0
@@ -37,9 +37,11 @@ ball_rect = ball_surface.get_rect(center = (WIDTH//2, HEIGHT//2))
 
 
 #font
-retro_font = pygame.font.Font('retrofont.ttf',45)
-score_surface = retro_font.render(f"{paddle_score_1} : {paddle_score_2}",True,black_color)
-score_rect = score_surface.get_rect(center = (WIDTH//2,50))
+def display_score():
+    retro_font = pygame.font.Font('retrofont.ttf',45)
+    score_surface = retro_font.render(f"{paddle_score_1} : {paddle_score_2}",True,black_color)
+    score_rect = score_surface.get_rect(center = (WIDTH//2,50))
+    screen.blit(score_surface,score_rect)
 
 # GAME LOOP STARTS
 while running:
@@ -67,11 +69,27 @@ while running:
   
     #ball movement
     ball_rect.x += ball_speed_x
+    
+    #if ball hits the paddle 
+    if ball_rect.colliderect(paddle_rect_1):
+        ball_speed_x *= -1
+    if ball_rect.colliderect(paddle_rect_2):
+        ball_speed_x *= -1
+        
+
+    #if ball goes to either wall, then increase the score respectively
+    if ball_rect.x > WIDTH :
+        paddle_score_1 += 1
+        ball_rect.center = (WIDTH//2,random.randrange(100,HEIGHT-100))
+    if ball_rect.x < 0:
+        paddle_score_2 += 1
+        ball_rect.center = (WIDTH//2, random.randrange(100,HEIGHT-100))
+
+    
     screen.fill("white")
-   
     screen.blit(paddle_surface_1,paddle_rect_1) #bliting paddle 1
     screen.blit(paddle_surface_2,paddle_rect_2) #bliting paddl 2
-    screen.blit(score_surface,score_rect)       # bliting score surface
+    display_score()
     screen.blit(ball_surface,ball_rect)
     pygame.display.update()
     clock.tick(60)
